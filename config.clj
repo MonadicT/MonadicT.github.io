ac3eae4ecac696843dbd1dfa1f75f6d7b29f16b9
Praki Prakash <praki.prakash@gmail.com>
2014-03-10 15:48:12 -0700

    rediect to _site/index

--- config.clj
+++ config.clj
@@ -2,7 +2,7 @@
  :site-description ""
  :site-url "http://MonadicT.github.io"
  :in-dir "_resources/"
- :out-dir "_site/"
+ :out-dir "_site"
  :default-template "default.clj"
  :encoding "UTF-8"
  :blog-as-index false
