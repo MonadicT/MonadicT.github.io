237d329407763c34e69f67ee07ca6efd6eb28fe4
Praki Prakash <Praki.Prakash@gmail.com>
2016-11-27 15:49:14 -0800

    Add Google analytics code

--- _resources/templates/default.clj
+++ _resources/templates/default.clj
@@ -22,7 +22,17 @@
 
   [:script {:src "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
             :type "text/javascript"}]
-  [:title (:title metadata)]]
+  [:script {:type "text/javascript"}
+  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88106602-1', 'auto');
+  ga('send', 'pageview');"
+  ]
+
+ [:title (:title metadata)]]
 
  [:div#wrap
  [:body
